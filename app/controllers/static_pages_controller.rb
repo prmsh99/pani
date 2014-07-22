26d099649b34e0cd9a1b322a6a4c9dc44f1cd05f
VinilChandra <vinilchandraranga@gmail.com>
2014-07-22 18:43:53 +0530

    static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,35 @@
+class StaticPagesController < ApplicationController
+  def jobs
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+  def csr
+  end
+
+  def careers
+  end
+
+  def company
+  end
+
+  def contact
+  end
+
+
+  def faq
+  end
+
+  def team
+
+  end
+
+ def training
+ end
+
+
+end
